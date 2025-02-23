dff9aea10698ed35391abe4aad12b236fbab2786
Leon <rahatsheikhlion@gmial.com>
2025-02-23 13:18:25 +0600

    data add system update

--- src/AddPeople.jsx
+++ src/AddPeople.jsx
@@ -3,19 +3,39 @@ import { useState } from "react"
 export function AddPeople() {
     // [memberInfoArr, setMemberInfoinArr]
     const [memberInfoArr, setMemberInfoinArr] = useState(
-        [
-            {
-                memberName: '',
-                memberPhone: '',
-                memberEmail: '',
-                memberNID: ''
-            }
-        ]
+
+        {
+            memberName: '',
+            memberPhone: '',
+            memberEmail: '',
+            memberNID: ''
+        }
+
 
     )
 
-    function test(){
+    function test() {
+        // console.log(memberInfoArr)
+        if (memberInfoArr.memberName === '' || memberInfoArr.memberPhone == '' || memberInfoArr.memberEmail == '' || memberInfoArr.memberNID == '') {
+            alert('Enter data')
+            console.log('test')
+            return;
+        }
         console.log(memberInfoArr)
+        setMemberInfoinArr({
+            memberName: '',
+            memberPhone: '',
+            memberEmail: '',
+            memberNID: ''
+        });
+    }
+
+    const handelChange = (e) => {
+        const { name, value } = e.target;
+        setMemberInfoinArr(prevState => ({
+            ...prevState,
+            [name]: value
+        }))
     }
 
     return (
@@ -25,29 +45,33 @@ export function AddPeople() {
                 <input type="text"
                     placeholder="Enter The Member's Name"
                     className="form-control"
+                    name="memberName"
                     value={memberInfoArr.memberName}
-                    onChange={(e) => setMemberInfoinArr(e.target.value)}
+                    onChange={(e) => handelChange(e, 'memberName')}
                 />
                 <input type="number"
                     placeholder="Enter The Member's Phone Number"
                     className="form-control"
+                    name="memberPhone"
                     value={memberInfoArr.memberPhone}
-                    onChange={(e) => setMemberInfoinArr(e.target.value)}
+                    onChange={(e) => handelChange(e, 'memberPhone')}
                 />
                 <input type="email"
                     placeholder="Enter The Member's Email"
                     className="form-control"
+                    name="memberEmail"
                     value={memberInfoArr.memberEmail}
-                    onChange={(e) => setMemberInfoinArr(e.target.value)}
+                    onChange={(e) => handelChange(e, 'memberEmail')}
                 />
                 <input type="number"
                     placeholder="Enter The Member's NID"
                     className="form-control"
+                    name="memberNID"
                     value={memberInfoArr.memberNID}
-                    onChange={(e) => setMemberInfoinArr(e.target.value)}
+                    onChange={(e) => handelChange(e, 'memberNID')}
                 />
 
-                <button type="button" onClick={test()}>Add Info</button>
+                <button type="button" onClick={test}>Add Info</button>
             </form>
         </>
     )
